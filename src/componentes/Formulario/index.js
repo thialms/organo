bd71a53377a4d881d2da308a8a1fb6e06324c82f
thialms <thiiago.allmeida44@gmail.com>
2025-01-07 14:58:57 -0300

    ReactClassAlura

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,39 @@
+import Botao from "../Botao";
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import "./Formulario.css";
+
+const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
+                <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Time "itens={times}/>
+                <Botao>
+                    Criar card
+                </Botao>
+            </form>
+        </section>
+    );
+};
+
+export default Formulario;
