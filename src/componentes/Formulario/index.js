f66022a161e9c967bf1c86dbcd288ddbe860263a
thialms <thiiago.allmeida44@gmail.com>
2025-01-09 21:32:04 -0300

    aula-04-finalizada

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -20,6 +20,10 @@ const Formulario = (props) => {
             imagem,
             time
         })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return (
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,10 +1,15 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
     return (
-            <section className='time' style={{backgroundColor: props.corSecundaria}}>
-                <h3 style={{borderColor: props.corPrimaria }}>{props.nome}</h3>
-            </section>
+        //renderização condicional, se o primeiro parametro for verdadeiro, retorna o segundo, outra maneira de fazer é utilizando o operador ternario
+        props.colaboradores.length > 0 && <section className='time' style={{ backgroundColor: props.corSecundaria }}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
+            </div>
+        </section>
     )
 }
 
